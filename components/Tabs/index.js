8dec875121a7483fda58e7c558dfda0f30d903bb
Dreamer <rwang24@student.cccd.edu>
2020-01-17 10:52:40 -0600

    Tabs complete

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,26 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+// axios get requests and console.log response
+axios
+  .get('https://lambda-times-backend.herokuapp.com/topics')
+  .then(response => {
+    console.log(response);
+
+    // Creating .topics component:
+    const topics = document.querySelector('.topics');
+
+    // Creating and Iterating through topics:
+    response.data.topics.forEach(topic => {
+      const tab = document.createElement('div');
+      tab.classList.add('tab');
+      tab.textContent = topic;
+      topics.append(tab);
+    })
+  })
+  
+  // Catch error
+  .catch(error => {
+    console.log(error);
+  })
